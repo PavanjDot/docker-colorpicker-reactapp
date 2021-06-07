e7aa09d57a9ad5895a73bc853e47a11920886c92
PavanjDot <pavanjdot@gmail.com>
2021-06-07 13:55:23 +0530

    First commit

---
+++ src/Random.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {Button} from './Button';
+ 
+export default class Random extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {color : [244, 24, 34]}
+    
+    this.handleClick = this.handleClick.bind(this);
+  }
+  componentDidMount() {
+    this.applyColor();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.applyColor();
+  }
+
+  formatColor(ary) {
+    return 'rgb(' + ary.join(', ') + ')';
+  }
+
+  isLight() {
+    const rgb = this.state.color;
+    return rgb.reduce((a,b) => a+b) < 127 * 3;
+  }
+
+  applyColor() {
+    const color = this.formatColor(this.state.color);
+    document.body.style.background = color;
+  }
+
+  chooseColor() {
+    const random = [];
+    for (let i = 0; i < 3; i++) {
+      random.push(Math.floor(Math.random()*256));
+    }
+    return random;
+  }
+  
+  handleClick(){
+    this.setState({color: this.chooseColor()});
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className={this.isLight() ? 'white' : 'black'}>
+          Youre Color is {this.formatColor(this.state.color)}
+
+        </h1>
+       <Button onClick = {this.handleClick}>Click me!</Button>
+      </div>
+    );
+  }
+}
+
+
+
+
